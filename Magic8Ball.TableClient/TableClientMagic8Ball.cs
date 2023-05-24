264c49699d1beef7c27eb34234ef607960d733e0
deanwiles <deanwiles@yahoo.com>
2023-05-24 12:06:22 -0700

    WIP

--- Magic8Ball.TableClient/TableClientMagic8Ball.cs
+++ Magic8Ball.TableClient/TableClientMagic8Ball.cs
@@ -16,8 +16,8 @@ namespace Magic8Ball.TableClient
             // Catch and log any errors
             try
             {
-                // TODO: Get Classic Answer from the Azure Data Table
-                // e.g. https://storageaccountmagic8d46.table.core.windows.net/Magic8BallAnswers?$filter=(PartitionKey%20eq%20'Classic')&$select=Type,Answer&st=2021-10-27T22%3A30%3A38Z&se=2021-10-28T22%3A30%3A38Z&sp=r&sv=2018-03-28&tn=magic8ballanswers&sig=UWGRQ3hiwv2%2FZhGiF4IitW3vg8A6S40fo%2BUFB%2BqWyHQ%3D
+                // TODO: Get Classic Answer from the Azure Data Table; GetConnectionStrings("Magic8BallDB")
+                // e.g. https://storageaccountmagic8d46.table.core.windows.net/Magic8BallAnswers?$filter=(PartitionKey%20eq%20'Classic')&$select=Type,Answer&sv=2020-08-04&ss=t&srt=o&sp=rl&se=2030-10-31T08:00:00Z&st=2021-10-29T18:40:26Z&spr=https&sig=VdvRqJWo5DL%2BAiB%2FyxW9pPjWpzIRjTm%2FHS6xVLeNE2I%3D?sv=2020-08-04&ss=t&srt=o&sp=rl&se=2030-10-31T08:00:00Z&st=2021-10-29T18:40:26Z&spr=https&sig=VdvRqJWo5DL%2BAiB%2FyxW9pPjWpzIRjTm%2FHS6xVLeNE2I%3D
                 // TODO: Then parse them into a list of answers and get and return a random answer from the list
                 throw new NotImplementedException();
             }
