9bab3a0291245e707e326f19f3cdf2aecfaecc98
Dean Wiles <deanwiles@yahoo.com>
2023-05-31 14:48:27 -0700

    Regen'd Magic8Ball.Shared project with minor cleanup

--- Magic8Ball.Shared/IMagic8BallService.cs
+++ Magic8Ball.Shared/IMagic8BallService.cs
@@ -1,14 +1,11 @@
-﻿using System.Threading.Tasks;
+﻿namespace Magic8Ball.Shared;
 
-namespace Magic8Ball.Shared
+public interface IMagic8BallService
 {
-     public interface IMagic8BallService
-    {
-        /// <summary>
-        /// Ask the Magic 8 Ball a Question
-        /// </summary>
-        /// <param name="Question">Yes/No question to ask</param>
-        /// <returns>The Magic 8 Ball object with resulting Question, Answer and Type</returns>
-        public Task<Magic8BallData> AskAsync(string Question);
-    }
+    /// <summary>
+    /// Ask the Magic 8 Ball a Question
+    /// </summary>
+    /// <param name="Question">Yes/No question to ask</param>
+    /// <returns>The Magic 8 Ball object with resulting Question, Answer and Type</returns>
+    public Task<Magic8BallData> AskAsync(string Question);
 }
--- Magic8Ball.Shared/Magic8BallData.cs
+++ Magic8Ball.Shared/Magic8BallData.cs
@@ -1,88 +1,88 @@
-﻿namespace Magic8Ball.Shared
+﻿namespace Magic8Ball.Shared;
+
+/// <summary>
+/// Type of Magic 8 Ball Answer
+/// </summary>
+public enum AnswerType
 {
+    /// <summary>
+    /// Negative / Contrary Answer
+    /// </summary>
+    Negative = -1,
+    /// <summary>
+    /// Neutral / Non-committal Answer
+    /// </summary>
+    Neutral = 0,
+    /// <summary>
+    /// Positive / Affirmative Answer
+    /// </summary>
+    Positive = 1,
+}
+
+/// <summary>
+/// Magic 8 Ball Answer
+/// </summary>
+public class MagicAnswer
+{
+    /// <summary>
+    /// Answer from Magic 8 Ball
+    /// </summary>
+    public string Answer { get; set; } = string.Empty;
+
     /// <summary>
     /// Type of Magic 8 Ball Answer
     /// </summary>
-    public enum AnswerType
+    /// <remarks>
+    /// Default type is Neutral
+    /// </remarks>
+    public AnswerType Type { get; set; } = AnswerType.Neutral;
+
+    /// <summary>
+    /// Magic 8 Ball Answer Default Constructor
+    /// </summary>
+    public MagicAnswer()
     {
-        /// <summary>
-        /// Negative / Contrary Answer
-        /// </summary>
-        Negative = -1,
-        /// <summary>
-        /// Neutral / Non-committal Answer
-        /// </summary>
-        Neutral = 0,
-        /// <summary>
-        /// Positive / Affirmative Answer
-        /// </summary>
-        Positive = 1,
     }
 
     /// <summary>
-    /// Magic 8 Ball Answer
+    /// Magic 8 Ball Answer Constructor with Values
     /// </summary>
-    public class MagicAnswer
+    /// <param name="Type">Type of Magic 8 Ball Answer</param>
+    /// <param name="Answer">Answer from Magic 8 Ball</param>
+    public MagicAnswer(AnswerType Type, string Answer)
     {
-        /// <summary>
-        /// Answer from Magic 8 Ball
-        /// </summary>
-        public string Answer { get; set; }
+        // Save Answer and Type
+        this.Type = Type;
+        this.Answer = Answer;
+    }
+}
 
-        /// <summary>
-        /// Type of Magic 8 Ball Answer
-        /// </summary>
-        public AnswerType Type { get; set; }
+/// <summary>
+/// Magic 8 Ball Question and Answer base class
+/// </summary>
+public abstract class Magic8BallData : MagicAnswer
+{
+    /// <summary>
+    /// Question asked of Magic 8 Ball
+    /// </summary>
+    public string Question { get; set; } = string.Empty;
 
-        /// <summary>
-        /// Magic 8 Ball Answer Default Constructor
-        /// </summary>
-        public MagicAnswer()
-        {
-            // Default type is Neutral
-            this.Type = AnswerType.Neutral;
-        }
-
-        /// <summary>
-        /// Magic 8 Ball Answer Constructor with Values
-        /// </summary>
-        /// <param name="Type">Type of Magic 8 Ball Answer</param>
-        /// <param name="Answer">Answer from Magic 8 Ball</param>
-        public MagicAnswer(AnswerType Type, string Answer)
-        {
-            // Save Answer and Type
-            this.Type = Type;
-            this.Answer = Answer;
-        }
+    /// <summary>
+    /// Magic 8 Ball Question and Answer Default Constructor
+    /// </summary>
+    public Magic8BallData() : base()
+    {
     }
 
     /// <summary>
-    /// Magic 8 Ball Question and Answer base class
+    /// Magic 8 Ball Question and Answer Constructor with Values
     /// </summary>
-    public abstract class Magic8BallData : MagicAnswer
+    /// <param name="Question">Yes/No question to ask</param>
+    /// <param name="Answer">Answer from Magic 8 Ball</param>
+    /// <param name="Type">Type of Magic 8 Ball Answer</param>
+    public Magic8BallData(string Question, string Answer, AnswerType Type) : base(Type, Answer)
     {
-        /// <summary>
-        /// Question asked of Magic 8 Ball
-        /// </summary>
-        public string Question { get; set; }
-
-        /// <summary>
-        /// Magic 8 Ball Question and Answer Default Constructor
-        /// </summary>
-        public Magic8BallData() : base()
-        {
-        }
-
-        /// <summary>
-        /// Magic 8 Ball Question and Answer Constructor with Values
-        /// </summary>
-        /// <param name="Question">Yes/No question to ask</param>
-        /// <param name="Answer">Answer from Magic 8 Ball</param>
-        /// <param name="Type">Type of Magic 8 Ball Answer</param>
-        public Magic8BallData(string Question, string Answer, AnswerType Type) : base(Type, Answer)
-        {
-            // Also save Question
-            this.Question = Question;
-        }
+        // Also save Question
+        this.Question = Question;
     }
 }
