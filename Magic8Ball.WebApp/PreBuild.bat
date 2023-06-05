b7b9dc2581844c7018d466aa5f79d42355b7f9aa
Dean Wiles <deanwiles@yahoo.com>
2023-06-05 15:43:23 -0700

    Update Magic8Ball.WebApp

--- Magic8Ball.WebApp/PreBuild.bat
+++ Magic8Ball.WebApp/PreBuild.bat
@@ -5,4 +5,3 @@ xcopy /y "%~dp0..\Images\Rotating Magic 8 Ball.gif" "%~dp0wwwroot\images\"
 xcopy /y "%~dp0..\Images\favicon_package_v0.16\*.png" "%~dp0wwwroot\images\"
 xcopy /y "%~dp0..\Images\favicon_package_v0.16\*.ico" "%~dp0wwwroot\images\"
 xcopy /y "%~dp0..\Images\favicon_package_v0.16\*.svg" "%~dp0wwwroot\images\"
-pause
--- Magic8Ball.WebApp/Program.cs
+++ Magic8Ball.WebApp/Program.cs
@@ -1,20 +1,11 @@
+using Magic8Ball.WebApp;
+using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Net.Http;
-using System.Threading.Tasks;
 
-namespace Magic8Ball.WebApp;
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
 
-public class Program
-{
-    public static async Task Main(string[] args)
-    {
-        var builder = WebAssemblyHostBuilder.CreateDefault(args);
-        builder.RootComponents.Add<App>("#app");
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-        builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
-        await builder.Build().RunAsync();
-    }
-}
+await builder.Build().RunAsync();
