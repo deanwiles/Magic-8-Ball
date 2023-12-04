4d06588a4ea5be43a7d94b051456667ca91da361
Dean Wiles <deanwiles@yahoo.com>
2023-12-04 12:16:18 -0800

    WIP use ASP.NET Core integration for isolated Azure functions

--- Magic8Ball.Api/App.cs
+++ Magic8Ball.Api/App.cs
@@ -1,30 +1,26 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
-using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
-using System.Net;
 
 namespace Magic8Ball.Api;
 
-public class App(ILogger<App> log)
+public class App(ILogger<App> logger)
 {
     [Function("App")]
-    public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Anonymous)] HttpRequestData request)
+    public IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequest request)
     {
-        log.LogInformation("C# HTTP trigger function processing an 'App' request...");
+        logger.LogInformation("C# HTTP trigger function processing an 'App' request...");
 
         // Redirect to Magic 8 Ball Azure Static Web App
-        var response = request.CreateResponse();
-        string setting = Environment.UserInteractive ? "MAGIC8BALL_WEBAPP_URL_DEV" : "MAGIC8BALL_WEBAPP_URL";
-        string? Url = Environment.GetEnvironmentVariable(setting);
-        if (Url == null)
+        var setting = Environment.UserInteractive ? "MAGIC8BALL_WEBAPP_URL_DEV" : "MAGIC8BALL_WEBAPP_URL";
+        var url = Environment.GetEnvironmentVariable(setting);
+        if (url == null)
         {
-            log.LogInformation($"Error: Missing environment variable \"{setting}\"");
-            response.StatusCode = HttpStatusCode.NotFound;
-            return response;
+            logger.LogInformation("Error: Missing environment variable \"{Setting}\"", setting);
+            return new NotFoundResult();
         }
-        log.LogInformation($"Redirecting to Url = \"{Url}\"...");
-        response.StatusCode = HttpStatusCode.Redirect;
-        response.Headers.Add("Location", Url);
-        return response;
+        logger.LogInformation("Redirecting to url = \"{Url}\"...", url);
+        return new RedirectResult(url);
     }
 }
