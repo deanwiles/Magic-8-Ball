5169176590244e5e03c841c7a3a6d99b75f4afd3
Dean Wiles <deanwiles@yahoo.com>
2023-12-04 14:21:32 -0800

    Completed Api updates for .NET 8 isolated Azure Function

--- Magic8Ball.Api/Ask.cs
+++ Magic8Ball.Api/Ask.cs
@@ -1,43 +1,34 @@
 using Magic8Ball.Classic;
 using Magic8Ball.Shared;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
-using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Converters;
-using Newtonsoft.Json.Serialization;
-using System;
-using System.IO;
-using System.Net;
-using System.Threading.Tasks;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace Magic8Ball.Api;
 
-public class Ask
+public class Ask(ILogger<Ask> logger)
 {
-    private readonly ILogger<Ask> _logger;
-
-    public Ask(ILogger<Ask> log)
-    {
-        _logger = log;
-    }
-
     [Function("Ask")]
     public async Task<IActionResult> Run(
-        [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequestData request)
+        [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest request)
     {
-        _logger.LogInformation("C# HTTP trigger function processed an 'Ask' request...");
+        logger.LogInformation("C# HTTP trigger function processed an 'Ask' request...");
 
         // Check for Question in GET query or in POST body
         string? question = request.Query["question"];
         if (string.IsNullOrWhiteSpace(question))
         {
             string requestBody = await new StreamReader(request.Body).ReadToEndAsync();
-            dynamic? data = JsonConvert.DeserializeObject(requestBody);
-            question = data?.question;
+            if (!string.IsNullOrWhiteSpace(requestBody))
+            {
+                dynamic? data = JsonSerializer.Deserialize<dynamic>(requestBody);
+                question = data?.question;
+            }
         }
-        _logger.LogInformation($"Question = \"{question}\"");
+        logger.LogInformation("Question = \"{Question}\"", question);
 
         // Ask the Classic Magic 8 Ball service the provided question
         var magic8Ball = new ClassicMagic8Ball();
@@ -46,24 +37,24 @@ public class Ask
             if (string.IsNullOrWhiteSpace(question))
                 throw new Exception("The Magic 8 Ball can only provide an Answer when a Question is asked.");
             await magic8Ball.AskAsync(question);
-            _logger.LogInformation($"Answer = \"{magic8Ball.Answer}\"");
-            _logger.LogInformation($"Type = \"{magic8Ball.Type}\"");
+            logger.LogInformation("Answer = \"{Answer}\"", magic8Ball.Answer);
+            logger.LogInformation("Type = \"{Type}\"", magic8Ball.Type);
         }
         catch (Exception ex)
         {
             // Return error message
             string msg = ex.Message;
             if (null != ex.InnerException) msg += $"{Environment.NewLine}{ex.InnerException.Message}";
-            _logger.LogInformation($"Error = \"{msg}\"");
+            logger.LogInformation("Error = \"{Error}\"", msg);
             return new BadRequestObjectResult(msg);
         }
 
         // Create JSON serializer that indents and uses camelCase property names and enumerations
-        var settings = new JsonSerializerSettings
+        var settings = new JsonSerializerOptions()
         {
-            Formatting = Formatting.Indented,
-            ContractResolver = new DefaultContractResolver { NamingStrategy = new CamelCaseNamingStrategy() },
-            Converters = { new StringEnumConverter(new CamelCaseNamingStrategy()) }
+            WriteIndented = true,
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+            Converters = { new JsonStringEnumConverter(JsonNamingPolicy.CamelCase) }
         };
 
         // Return Magic8Ball object as JSON
