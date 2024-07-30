412768a90f0a2c0567e2cb1d397fbc7f0a0a5fc1
Dean Wiles <deanwiles@yahoo.com>
2024-07-30 15:08:57 -0700

    Add initial xUnit tests and implement some compiler recommended code simplifications

---
+++ Magic8Ball.AITests/AIMagic8BallTests.cs
@@ -0,0 +1,52 @@
+namespace Magic8Ball.AITests
+{
+    using System;
+    using System.Threading.Tasks;
+    using Magic8Ball.AI;
+    using Xunit;
+
+    public class AIMagic8BallTests
+    {
+        private readonly AIMagic8Ball _testClass;
+
+        public AIMagic8BallTests()
+        {
+            _testClass = new AIMagic8Ball();
+        }
+
+        [Fact]
+        public async Task CanCallAskAsync()
+        {
+            // Arrange
+            var Question = "TestValue757066526";
+
+            // Act
+            var result = await _testClass.AskAsync(Question);
+
+            // Assert non-empty Answer received
+            Assert.False(string.IsNullOrEmpty(result.Answer));
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData("   ")]
+        public async Task CannotCallAskAsyncWithInvalidQuestion(string value)
+        {
+            await Assert.ThrowsAsync<Exception>(() => _testClass.AskAsync(value));
+        }
+
+        [Fact]
+        public async Task AskAsyncPerformsMapping()
+        {
+            // Arrange
+            var Question = "TestValue533726191";
+
+            // Act
+            var result = await _testClass.AskAsync(Question);
+
+            // Assert
+            Assert.Same(Question, result.Question);
+        }
+    }
+}
--- Magic8Ball.Classic/ClassicMagic8Ball.cs
+++ Magic8Ball.Classic/ClassicMagic8Ball.cs
@@ -13,26 +13,26 @@ public class PredefinedMagicAnswers : ReadOnlyCollection<MagicAnswer>
     /// </summary>
     public static readonly PredefinedMagicAnswers ClassicAnswers = new(new List<MagicAnswer>()
     {
-        new MagicAnswer(AnswerType.Negative, "Don't count on it"),
-        new MagicAnswer(AnswerType.Negative, "My reply is no"),
-        new MagicAnswer(AnswerType.Negative, "My sources say no"),
-        new MagicAnswer(AnswerType.Negative, "Outlook not so good"),
-        new MagicAnswer(AnswerType.Negative, "Very doubtful"),
-        new MagicAnswer(AnswerType.Neutral, "Ask again later"),
-        new MagicAnswer(AnswerType.Neutral, "Better not tell you now"),
-        new MagicAnswer(AnswerType.Neutral, "Cannot predict now"),
-        new MagicAnswer(AnswerType.Neutral, "Concentrate and ask again"),
-        new MagicAnswer(AnswerType.Neutral, "Reply hazy, try again"),
-        new MagicAnswer(AnswerType.Positive, "As I see it, yes"),
-        new MagicAnswer(AnswerType.Positive, "It is certain"),
-        new MagicAnswer(AnswerType.Positive, "It is decidedly so"),
-        new MagicAnswer(AnswerType.Positive, "Most likely"),
-        new MagicAnswer(AnswerType.Positive, "Outlook good"),
-        new MagicAnswer(AnswerType.Positive, "Signs point to yes"),
-        new MagicAnswer(AnswerType.Positive, "Without a doubt"),
-        new MagicAnswer(AnswerType.Positive, "Yes"),
-        new MagicAnswer(AnswerType.Positive, "Yes, definitely"),
-        new MagicAnswer(AnswerType.Positive, "You may rely on it")
+        new(AnswerType.Negative, "Don't count on it"),
+        new(AnswerType.Negative, "My reply is no"),
+        new(AnswerType.Negative, "My sources say no"),
+        new(AnswerType.Negative, "Outlook not so good"),
+        new(AnswerType.Negative, "Very doubtful"),
+        new(AnswerType.Neutral, "Ask again later"),
+        new(AnswerType.Neutral, "Better not tell you now"),
+        new(AnswerType.Neutral, "Cannot predict now"),
+        new(AnswerType.Neutral, "Concentrate and ask again"),
+        new(AnswerType.Neutral, "Reply hazy, try again"),
+        new(AnswerType.Positive, "As I see it, yes"),
+        new(AnswerType.Positive, "It is certain"),
+        new(AnswerType.Positive, "It is decidedly so"),
+        new(AnswerType.Positive, "Most likely"),
+        new(AnswerType.Positive, "Outlook good"),
+        new(AnswerType.Positive, "Signs point to yes"),
+        new(AnswerType.Positive, "Without a doubt"),
+        new(AnswerType.Positive, "Yes"),
+        new(AnswerType.Positive, "Yes, definitely"),
+        new(AnswerType.Positive, "You may rely on it")
     }.AsReadOnly());
 
     /// <summary>
@@ -73,15 +73,15 @@ public class GroupedMagicAnswers
     /// <summary>
     /// Negative / Contrary Answers
     /// </summary>
-    public List<string> Negative { get; set; } = new();
+    public List<string> Negative { get; set; } = [];
     /// <summary>
     /// Neutral / Non-committal Answers
     /// </summary>
-    public List<string> Neutral { get; set; } = new(); 
+    public List<string> Neutral { get; set; } = [];
     /// <summary>
     /// Positive / Affirmative Answers
     /// </summary>
-    public List<string> Positive { get; set; } = new();
+    public List<string> Positive { get; set; } = [];
 
     /// <summary>
     /// Return the Collection of Magic 8 Ball Answers grouped by AnswerType as a Predefined List of MagicAnswers
@@ -141,7 +141,7 @@ public class ClassicMagic8Ball : Magic8BallData, IMagic8BallService
         {
             // If no question, force an error to demonstrate inner exception handling
             if (string.IsNullOrWhiteSpace(Question))
-                throw new Exception("You must ask a question to get an answer!");
+                throw new ArgumentException("You must ask a question to get an answer!");
             // Get random answer
             int index = _random.Next(_answers.Count);
             var answer = _answers[index];
